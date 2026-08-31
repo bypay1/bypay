fbd1cef5ef886c4b1b05f9e89d5419a20df4d5a1
bypay1 <johnpaulljude99@gmail.com>
2026-08-31 13:50:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="bypay-v2";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>fetch(e.request))));
